6bac2ab0081869e43114d3621fb96ca6a23a584b
Andreas Källberg <anka.213@gmail.com>
2022-04-06 14:15:29 +0800

    Add more documentation

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,22 +1,42 @@
+{- |
+A very simple binding for udpipe.
+
+Example usage:
+
+> import UDPipe
+>
+> main = do
+>   let sentence = "The quick brown fox jumps over the lazy dog."
+>   let modelFile = "models/english-lines-ud-2.5-191206.udpipe"
+>   model' <- loadModel modelFile
+>   let model = case model' of
+>                 Left err -> error err
+>                 Right m -> m
+>   result <- runPipe model sentence
+>   putStrLn result
+
+
+-}
 module MyLib (runExample, runPipeline, loadModel, ModelPtr) where
 import Foreign
 import Foreign.C
 
+-- | A loaded udpipe model.
 type ModelPtr = Ptr Model
 
 data Model
 
 foreign import ccall unsafe "load_model" cLoadModel
-  :: CString -> IO (Ptr Model)
+  :: CString -> IO ModelPtr
 
 foreign import ccall unsafe "run_pipeline_simple" cRunPipeline
-  :: Ptr Model -> CString -> IO CString
+  :: ModelPtr -> CString -> IO CString
 
 foreign import ccall unsafe "free_cstr" freeCString
   :: CString -> IO ()
 
 -- | Run the default ud pipeline on an input string.
-runPipeline :: Ptr Model -> String -> IO String
+runPipeline :: ModelPtr -> String -> IO String
 runPipeline model input = do
   withCString input $ \cInput -> do
     cOutput <- cRunPipeline model cInput
@@ -33,9 +53,9 @@ loadModel path = do
       then return $ Left $ "Failed to load model from " ++ path
       else return $ Right model
 
+-- | An example of how to use the library.
 runExample :: IO ()
 runExample = do
-    -- TODO: Free data (e.g. using withCString)
     let modelName = "models/english-lines-ud-2.5-191206.udpipe"
     mod <- loadModel modelName
     case mod of
@@ -44,6 +64,5 @@ runExample = do
         let input = "This is an example"
         ans <- runPipeline mod input
         putStrLn ans
-        putStrLn "runExample"
 
 -- TODO: Handle errors in the C++ code (give back error message)
